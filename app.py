bd72c69af202a0ad7a5f785dc3be9633f60bb726
KaCee <kaceeunruhrdh@gmail.com>
2023-10-16 16:25:52 -0400

    Make Table interactive

--- app.py
+++ app.py
@@ -1,17 +1,64 @@
 from flask import Flask, render_template, jsonify
+import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_table
 import requests
 
-app = Flask(__name__)
+# Set up the app with external stylesheets from dash-bootstrap-components
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server  # This is for deployment
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# Define the app layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Button("Fetch Data", id="fetch-button", color="primary", className="mr-2"),
+        ])
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(
+                id='datatable',
+                columns=[
+                    {"name": "Agency ID", "id": "agency_id"},
+                    {"name": "Toptier Code", "id": "toptier_code"},
+                    {"name": "Agency Name", "id": "agency_name"},
+                    {"name": "Abbreviation", "id": "abbreviation"},
+                    {"name": "Congressional Justification URL", "id": "congressional_justification_url"},
+                    {"name": "Active FY", "id": "active_fy"},
+                    {"name": "Active FQ", "id": "active_fq"},
+                    {"name": "Outlay Amount", "id": "outlay_amount"},
+                    {"name": "Obligated Amount", "id": "obligated_amount"},
+                    {"name": "Budget Authority Amount", "id": "budget_authority_amount"},
+                    {"name": "Current Total Budget Authority Amount", "id": "current_total_budget_authority_amount"},
+                    {"name": "Percentage of Total Budget Authority", "id": "percentage_of_total_budget_authority"},
+                    {"name": "Agency Slug", "id": "agency_slug"}
+                ],
+                data=[]
+            ),
+        ])
+    ])
+], fluid=True)
 
-@app.route("/fetch_data")
-def fetch_data():
+# Define callback to update table data
+@app.callback(
+    Output('datatable', 'data'),
+    Input('fetch-button', 'n_clicks')
+)
+def update_table(n_clicks):
+    if not n_clicks:
+        return []
+
     url = "https://api.usaspending.gov/api/v2/references/toptier_agencies/"
     response = requests.get(url)
-    return jsonify(response.json()['results'])
 
-    if __name__ == '__main__':
-        app.run()
+    if response.status_code == 200:
+        return response.json()['results']
+    else:
+        return []
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
