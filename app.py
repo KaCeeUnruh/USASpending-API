9cbdd4a5d050d4159fd806b79e27c1841ca24e71
KaCee <kaceeunruhrdh@gmail.com>
2023-10-23 19:35:39 -0400

    rows per page alteration

--- app.py
+++ app.py
@@ -7,63 +7,41 @@ from dash.dependencies import Input, Output
 import requests
 import pandas as pd
 
-# Define the function to fetch data and change column names
+# Define the function to fetch data
 def fetch_data():
     url = "https://api.usaspending.gov/api/v2/references/toptier_agencies/"
     response = requests.get(url)
     data = response.json()
-    df = pd.DataFrame(data['results'])
-    df = df.rename(columns={
-        "agency_id": "Agency ID",
-        "toptier_code": "Toptier Code",
-        "agency_name": "Agency Name",
-        "abbreviation": "Abbreviation",
-        "congressional_justification_url": "Congressional Justification URL",
-        "active_fy": "Active FY",
-        "active_fq": "Active FQ",
-        "outlay_amount": "Outlay Amount",
-        "obligated_amount": "Obligated Amount",
-        "budget_authority_amount": "Budget Authority Amount",
-        "current_total_budget_authority_amount": "Current Total Budget Authority Amount",
-        "percentage_of_total_budget_authority": "Percentage of Total Budget Authority",
-        "agency_slug": "Agency Slug"
-    })
-    return df
+    return pd.DataFrame(data['results'])
 
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
-server = app.server
+server = app.server  # This is needed for Heroku deployment
 
 # Define the app layout
 app.layout = html.Div([
-    html.H1("USA Government Spending", style={"textAlign": "center", "fontSize": 40}),
+    html.H1("USA Government Spending", style={"textAlign": "center", "fontSize": 24}),
     dbc.Button("Fetch Data", id="fetch-button", className="mb-3"),
     dash_table.DataTable(
         id='data-table',
         columns=[{'name': i, 'id': i} for i in []], # Initially empty
         page_size=10,  # Number of rows per page
         style_table={'fontSize': 12},
+    ),
 ])
 
 # Define callback to update table
 @app.callback(
-    Output("data-table", "children"),
-    [Input("fetch-button", "n_clicks"), Input("page-dropdown", "value")]
+    Output("data-table", "data"),
+    Output("data-table", "columns"),
+    [Input("fetch-button", "n_clicks")]
 )
-
-def update_table(n_clicks, page_number):
+def update_table(n_clicks):
     if n_clicks:
         df = fetch_data()
-        # Implementing basic pagination
-        rows_per_page = 10  # Assuming 10 rows per page
-        start_idx = (page_number - 1) * rows_per_page
-        end_idx = start_idx + rows_per_page
-        df_page = df.iloc[start_idx:end_idx]
-        table_header = [html.Thead(html.Tr([html.Th(column) for column in df.columns]))]
-        table_body = [html.Tbody(
-            [html.Tr([html.Td(df_page.iloc[i][column]) for column in df.columns]) for i in range(len(df_page))])]
-        return table_header + table_body
-    return []
+        columns = [{"name": i, "id": i} for i in df.columns]
+        return df.to_dict('records'), columns
+    return [], []
 
 # Run the app
 if __name__ == "__main__":
