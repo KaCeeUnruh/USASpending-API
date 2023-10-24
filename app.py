4c6f2ffe69b53b3fe9af1dc2ecc53bde601aea79
KaCee <kaceeunruhrdh@gmail.com>
2023-10-24 08:18:42 -0400

    Change fetch button

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ server = app.server  # This is needed for Heroku deployment
 
 # Define the app layout
 app.layout = html.Div([
-    html.H1("USA Government Spending", style={"textAlign": "center", "fontSize": 40}),
-    dbc.Button("Fetch Data", id="fetch-button", className="mb-3"),
+    html.H1("USA Government Spending", style={"textAlign": "center", "fontSize": 50}),
+    dbc.Button("Fetch Data", id="fetch-button", className="mb-4", "textAlign": "center"),
     dash_table.DataTable(
         id='data-table',
         columns=[{'name': i, 'id': i} for i in []], # Initially empty
